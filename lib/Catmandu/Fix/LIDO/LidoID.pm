c381578f108f4a46036070467cf5951934ea8979
Pieter De Praetere <pieter@packed.be>
2016-10-06 08:21:14 +0200

    Moved lido_recid to lido_baseid to add support for objectPublishedID

---
+++ lib/Catmandu/Fix/LIDO/LidoID.pm
@@ -0,0 +1,38 @@
+package Catmandu::Fix::LIDO::LidoID;
+
+use strict;
+
+use Exporter qw(import);
+use Data::Dumper qw(Dumper);
+
+use Catmandu::Fix::LIDO::Utility qw(walk);
+
+our @EXPORT_OK = qw(generate_id);
+
+sub generate_id {
+    my ($fixer, $path, $cm_value, $source, $label, $type) = @_;
+    push @$path, '$append';
+    my $perl = '';
+
+    $perl .= $fixer->emit_create_path(
+		$fixer->var,
+		$path,
+		sub {
+			my $path_var = shift;
+			my $code = '';
+			$code .= "${path_var} = {"
+				."'_' => ${cm_value},"
+				."'type' => '".$type."',";
+			if (defined($source)) {
+				$code .= "'source' => '".$source."',";
+			}
+			$code .= "'label' => '".$label."'"
+				."};";
+			return $code;
+		}
+	);
+
+    return $perl;
+};
+
+1;
--- lib/Catmandu/Fix/lido_baseid.pm
+++ lib/Catmandu/Fix/lido_baseid.pm
@@ -1,26 +1,31 @@
-package Catmandu::Fix::lido_recid;
+package Catmandu::Fix::lido_baseid;
 
 use Catmandu::Sane;
 use Moo;
 use Catmandu::Fix::Has;
 use Catmandu::Fix::LIDO::Utility qw(walk);
+use Catmandu::Fix::LIDO::LidoID qw(generate_id);
 
 use strict;
 
 #https://librecatproject.wordpress.com/2014/03/26/create-a-fixer-part-2/
 
+use Data::Dumper qw(Dumper);
 
 with 'Catmandu::Fix::Base';
 
-has recid => ( fix_arg => 1 );
-has type => (fix_arg => 1);
+has path => (fix_arg => 1);
+has id_value => (fix_arg => 1);
+has type => (fix_opt => 1, default => sub { 'global' });
+has source => (fix_opt => 1);
+has label => (fix_opt => 1, default => sub { 'dataPID' });
 
 sub emit {
 	my ($self, $fixer) = @_;
-	my $recid_path = $fixer->split_path($self->recid);
+	my $recid_path = $fixer->split_path($self->id_value);
 	my $recid_key = pop @$recid_path;
 	
-	my $new_path = ['lidoRecID', '$append'];
+	my $new_path = $fixer->split_path($self->path);
 	
 	my $h = $fixer->generate_var();
 	my $perl = '';
@@ -28,17 +33,7 @@ sub emit {
 	
 	$perl .= walk($fixer, $recid_path, $recid_key, $h);
 	
-	$perl .= $fixer->emit_create_path(
-		$fixer->var,
-		$new_path,
-		sub {
-			my $path_var = shift;
-			return "${path_var} = {"
-				."'_' => ${h},"
-				."'type' => '".$self->type."'"
-				."};";
-		}
-	);
+	$perl .= generate_id($fixer, $new_path, $h, $self->source, $self->label, $self->type);
 	
 	$perl;
 }
