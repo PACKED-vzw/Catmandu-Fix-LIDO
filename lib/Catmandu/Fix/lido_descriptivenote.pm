50e9cd9550c37de66b204dfd07c9514d3e5b495f
Pieter De Praetere <pieter@packed.be>
2016-10-10 11:31:10 +0200

    Added descriptiveNoteValue

---
+++ lib/Catmandu/Fix/lido_descriptivenote.pm
@@ -0,0 +1,60 @@
+package Catmandu::Fix::lido_descriptivenote;
+
+use Catmandu::Sane;
+use Moo;
+use Catmandu::Fix::Has;
+use Catmandu::Fix::LIDO::Utility qw(walk declare_source);
+
+use strict;
+
+#https://librecatproject.wordpress.com/2014/03/26/create-a-fixer-part-2/
+
+use Data::Dumper qw(Dumper);
+
+with 'Catmandu::Fix::Base';
+
+has path => (fix_arg => 1);
+has value => (fix_arg => 1);
+has value_lang => (fix_opt => 1, default => sub { 'en' });
+has value_label => (fix_opt => 1, default => sub { 'description' });
+
+sub emit {
+    my ($self, $fixer) = @_;
+
+    my $perl = '';
+
+    my $h = $fixer->generate_var();
+    my $new_path = $fixer->split_path($self->path);
+
+    my $f_value = $fixer->generate_var();
+    $perl .= "my ${f_value};";
+    $perl .= declare_source($fixer, $self->value, $f_value);
+
+    $perl .= $fixer->emit_create_path(
+        $fixer->var,
+        $new_path,
+        sub {
+            my $root = shift;
+            my $r_code = '';
+
+            $r_code .= $fixer->emit_create_path(
+                $root,
+                ['$append', 'descriptiveNoteValue'],
+                sub {
+                    my $dn_root = shift;
+                    return "${dn_root} = {"
+                    ."'_' => ${f_value},"
+                    ."'lang' => '".$self->value_lang."',"
+                    ."'label' => '".$self->value_label."'"
+                    ."};";
+                }
+            );
+
+            return $r_code;
+        }
+    );
+
+    return $perl;
+}
+
+1;
