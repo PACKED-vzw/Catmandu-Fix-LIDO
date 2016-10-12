26d8508f73bb037ae614ece59d2ce6ec4451ca10
Pieter De Praetere <pieter@packed.be>
2016-10-12 10:31:55 +0200

    Added documentation

--- lib/Catmandu/Fix/lido_classification.pm
+++ lib/Catmandu/Fix/lido_classification.pm
@@ -19,11 +19,11 @@ has object_work_type        => (fix_arg => 1);
 has classification          => (fix_arg => 1);
 has object_work_type_id     => (fix_opt => 1);
 has classification_id       => (fix_opt => 1);
-has object_work_type_type   => (fix_opt => 1, default => sub { 'global' });
-has object_work_type_source => (fix_opt => 1, default => sub { 'AAT' });
-has classification_type     => (fix_opt => 1, default => sub { 'global' });
-has classification_source   => (fix_opt => 1, default => sub { 'AAT' });
-has lang                    => (fix_opt => 1, default => sub {'en'});
+has object_work_type_type   => (fix_opt => 1);
+has object_work_type_source => (fix_opt => 1);
+has classification_type     => (fix_opt => 1);
+has classification_source   => (fix_opt => 1);
+has lang                    => (fix_opt => 1);
 
 sub emit {
     my ($self, $fixer) = @_;
@@ -62,3 +62,104 @@ sub emit {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::lido_classification - create an C<objectClassificationWrap>.
+
+=head1 SYNOPSIS
+
+    lido_classification(
+        object_work_type,
+        classification,
+        object_work_type_id: objectWorkType.conceptID,
+        classification_id: classification.conceptID,
+        object_work_type_type: objectWorkType.type,
+        object_work_type_source: objectWorkType.source,
+        classification_type: classification.type,
+        classification_source: classification_source,
+        lang: objectClassificationWrap.*.lang
+    )
+
+=head1 DESCRIPTION
+
+C<lido_classification> will create a C<objectClassificationWrap> containing both the C<classificationWrap.classification> and the C<objectWorkTypeWrap.objectWorkType>.
+
+=head2 Parameters
+
+=head3 Required parameters
+
+C<object_work_type> and C<classification> are required path parameters.
+
+=over
+
+=item C<object_work_type>
+
+=item C<classification>
+
+=back
+
+=head3 Optional parameters
+
+C<object_work_type_id> and C<classification_id> are optional path parameters. All other parameters are strings. Note that C<lang> is repeated on both C<objectWorkType> and C<classification>.
+
+=over
+
+=item C<object_work_type_id>
+
+=item C<classification_id>
+
+=back
+
+=over
+
+=item C<object_work_type_type>
+
+=item C<object_work_type_source>
+
+=item C<classification_type>
+
+=item C<classification_source>
+
+=item C<lang>
+
+=back
+
+=head1 EXAMPLE
+
+=head2 Fix
+
+    lido_classification (
+        recordList.record.object_name.value,
+        recordList.record.object_cat.value,
+        -object_work_type_id: recordList.record.object_name.id,
+        -classification_id: recordList.record.object_cat.id,
+        -object_work_type_type: global,
+        -object_work_type_source: adlib,
+        -classification_type: global,
+        -classification_source: adlib,
+        -lang: nl
+    )
+
+=head2 Result
+
+    <lido:descriptiveMetadata>
+        <lido:objectClassificationWrap>
+            <lido:objectWorkTypeWrap>
+                <lido:objectWorkType>
+                    <lido:conceptID lido:type="global" lido:source="adlib">123</lido:conceptID>
+                    <lido:term xml:lang="nl">olieverfschilderij</lido:term>
+                </lido:objectWorkType>
+            </lido:objectWorkTypeWrap>
+            <lido:classificationWrap>
+                <lido:classification>
+                    <lido:conceptID lido:pref="preferred" lido:type="global" lido:source="adlib">123</lido:conceptID>
+                    <lido:term lido:pref="preferred" xml:lang="nl">Schilderijen</lido:term>
+                </lido:classification>
+            </lido:classificationWrap>
+        </lido:objectClassificationWrap>
+    </lido:descriptiveMetadata>
