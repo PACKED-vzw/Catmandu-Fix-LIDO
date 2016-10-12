50f1d5889a010f9fb93deee9ae0fcc2f74177498
Pieter De Praetere <pieter@packed.be>
2016-10-12 09:58:34 +0200

    Added documentation

--- lib/Catmandu/Fix/lido_basenameset.pm
+++ lib/Catmandu/Fix/lido_basenameset.pm
@@ -34,5 +34,87 @@ sub emit {
     return $perl;
 }
 
-
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::lido_basenameset - Create a basic nameset in a C<path>
+
+=head1 SYNOPSIS
+
+    lido_basenameset (
+        path,
+        value,
+        value_pref: appellationValue.pref,
+        value_lang: appellationValue.lang,
+        source: sourceAppellation,
+        source_lang: sourceAppellation.lang
+    )
+
+=head1 DESCRIPTION
+
+C<lido_basenameset> creates a basic LIDO node that contains both C<appellationValue> and C<sourceAppellation> at a specified C<path>.
+
+=head2 Parameters
+
+=head3 Required parameters
+
+C<path> and C<value> are required parameters that must be a path.
+
+=over
+
+=item C<path>
+
+=item C<value> (appellationValue)
+
+=back
+
+=head3 Optional parameters
+
+C<source> must be a path, all the other parameters are strings.
+
+=over
+
+=item C<source> (sourceAppellation)
+
+=back
+
+=over
+
+=item C<value_pref>
+
+=item C<value_lang>
+
+=item C<source_lang>
+
+=back
+
+=head1 EXAMPLE
+
+=head2 Fix
+
+    lido_basenameset(
+        descriptiveMetadata.objectIdentificationWrap.titleWrap.titleSet,
+        recordList.record.title.value,
+        -value_lang: nl,
+        -value_pref: preferred,
+        -source: recordList.record.title.source,
+        -source_lang: nl
+    )
+
+=head2 Result
+
+    <lido:descriptiveMetadata>
+        <lido:objectIdentificationWrap>
+            <lido:titleWrap>
+                <lido:titleSet>
+                    <lido:appellationValue lido:pref="preferred" xml:lang="nl">Naderend onweer</lido:appellationValue>
+                    <lido:sourceAppellation xml:lang="nl">MSK Gent</lido:sourceAppellation>
+                </lido:titleSet>
+            </lido:titleWrap>
+        </lido:objectIdentificationWrap>
+    </lido:descriptiveMetadata>
