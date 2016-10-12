66f62689d0d5c747886f89eec16c33f2e6abb3fc
Pieter De Praetere <pieter@packed.be>
2016-10-12 10:46:49 +0200

    Added documentation

--- lib/Catmandu/Fix/lido_descriptivenote.pm
+++ lib/Catmandu/Fix/lido_descriptivenote.pm
@@ -43,3 +43,73 @@ sub emit {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::lido_descriptivenote - create a C<descriptiveNoteValue> in a C<path>
+
+=head1 SYNOPSIS
+
+    lido_descriptivenote(
+        path,
+        value,
+        -lang: descriptiveNoteValue.lang,
+        -label: descriptiveNoteValue.label
+    )
+
+=head1 DESCRIPTION
+
+Creates a C<descriptiveNoteValue> in a C<path>.
+
+=head2 Parameters
+
+=head3 Required parameters
+
+C<path> and C<value> are required path parameters.
+
+=over
+
+=item C<path>
+
+=item C<value>
+
+=back
+
+=head3 Optional parameters
+
+All optional parameters are strings.
+
+=over
+
+=item C<lang>
+
+=item C<label>
+
+=back
+
+=head1 EXAMPLE
+
+=head2 Fix
+
+    lido_descriptivenote(
+        descriptiveMetadata.eventWrap.eventSet.$last.event.eventDescriptionSet,
+        recordList.record.creation_desc,
+        -lang: nl
+    )
+
+=head2 Result
+    <lido:descriptiveMetadata>
+        <lido:eventWrap>
+            <lido:eventSet>
+                <lido:event>
+                    <lido:eventDescriptionSet>
+                        <lido:descriptiveNoteValue xml:lang="nl">Een generieke beschrijving.</lido:descriptiveNoteValue>
+                    </lido:eventDescriptionSet>
+                </lido:event>
+            </lido:eventSet>
+        </lido:eventWrap>
+    </lido:descriptiveMetadata>
