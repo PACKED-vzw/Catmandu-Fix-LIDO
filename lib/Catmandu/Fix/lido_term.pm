203dbc7dc94f28c11b192accdfa3f3ef5c0ee92f
Pieter De Praetere <pieter@packed.be>
2016-10-12 11:20:47 +0200

    Added documentation

--- lib/Catmandu/Fix/lido_term.pm
+++ lib/Catmandu/Fix/lido_term.pm
@@ -34,3 +34,82 @@ sub emit {
 1;
 
 __END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Fix::lido_term - create a C<term> and C<conceptID> node in a C<path>
+
+=head1 SYNOPSIS
+
+    lido_term(
+        path,
+        term,
+        -conceptid: conceptID,
+        -lang: term.lang,
+        -pref: term.pref,
+        -source: conceptID.source,
+        -type: conceptID.type
+    )
+
+=head1 DESCRIPTION
+
+Create a node consisting of a C<term> and a C<conceptID> in a C<path>.
+
+=head2 Parameters
+
+=head3 Required parameters
+
+The parameters C<term> and C<path> are required path parameters.
+
+=over
+
+=item C<term>
+
+=item C<path>
+
+=back
+
+=head3 Optional parameters
+
+C<conceptid> is an optional path parameter.
+
+=over
+
+=item C<conceptid>
+
+=back
+
+All other optional parameters are strings.
+
+=over
+
+=item C<lang>
+
+=item C<pref>
+
+=item C<source>
+
+=item C<type>
+
+=back
+
+=head1 EXAMPLE
+
+=head2 Fix
+
+    lido_term(
+        category,
+        recordList.record.category.value,
+        -conceptid: recordList.record.category.id,
+        -type: global,
+        -source: 'cidoc-crm',
+    )
+
+=head2 Result
+
+    <lido:category>
+        <lido:conceptID lido:type="global" lido:source="cidoc-crm">123</lido:conceptID>
+        <lido:term>Paintings</lido:term>
+    </lido:category>
